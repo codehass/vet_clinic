d2d150ab1ce6b3ff368e29fdcc3f6d829c32453c
codehass <hassan.elouardy06@gmail.com>
2023-02-23 12:35:19 +0100

    Add data

--- data.sql
+++ data.sql
@@ -21,3 +21,27 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 ('Blossom', '1998-10-13', 3, true, 17),
 ('Agumon', '2022-05-14', 4, true, 22);
 
+/*Insert the data into the owners table*/
+INSERT INTO owners (full_name, age) VALUES 
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+/*Insert the data into the species table*/
+INSERT INTO species (name) VALUES 
+('Pokemon'),
+('Digimon');
+
+/*Update the animals table*/
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+UPDATE animals SET species_id = 2 WHERE name  LIKE '%mon';
+
+/*Update the animals table*/
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name='Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name='Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name='Squirtle' OR name='Blossom';
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name='Boarmon';
