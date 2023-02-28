064f490bcf8e4fb062b87e5422c74ef3cee588b4
codehass <hassan.elouardy06@gmail.com>
2023-02-28 14:41:34 +0100

    Add email column to owners table

--- schema.sql
+++ schema.sql
@@ -69,3 +69,8 @@ CREATE TABLE visits (
     FOREIGN KEY (animal_id) REFERENCES animals(id),
     FOREIGN KEY (vet_id) REFERENCES vets(id)
 );
+
+/* Performance */
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
